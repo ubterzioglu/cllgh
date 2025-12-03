f19e49a672cd491df4d889dfce4f3eccaf864330
ubterzioglu <ubterzioglu@gmail.com>
2025-12-03 15:34:01 +0100

    Initial commit

---
+++ LearningCSharp/Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Consol.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Consol.WriteLine("Why, hello there " + str);
